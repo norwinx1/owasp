2ea5f15ed1554e0ff2fbabc055b90b35a31eddda
Norwin Schäfer <norwin270@gmail.com>
2023-06-12 08:22:37 +0200

    add logging

--- src/main/java/ch/bbw/owasp/OwaspController.java
+++ src/main/java/ch/bbw/owasp/OwaspController.java
@@ -1,12 +1,23 @@
 package ch.bbw.owasp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
 @Controller
+@Slf4j
 public class OwaspController {
+    @RequestMapping("/")
+    String index(Principal principal) {
+        log.info("Serving index page for user: {}", principal.getName());
+        return "index";
+    }
+
     @RequestMapping("/login")
     String login() {
+        log.info("Serving login page");
         return "login";
     }
 }
